fdb9e7f872e3e107ee12ff98286e1d3c4a47527d
parke415 <parke415@gmail.com>
2020-12-06 13:59:04 -0800

    restructures app

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,5 +1,25 @@
 const Character = require('../models/character');
 
+// DUMMY DATA
+const charOne = new Character;
+charOne.glyph = "渥";
+charOne.components = [{glyph: '水', role: 'Semantic', form: '氵'}, {glyph: '屋', role: 'Phonetic'}];
+charOne.strokes = 12;
+charOne.learned = false;
+charOne.variants = '沃沃';
+const meaning1 = {definition: 'drip', readings: [{language: 'Mandarin', sound: 'wu1', register: 'literary'}, {language: 'Cantonese', sound: 'uk1'}]};
+const meaning2 = {definition: 'canal', readings: [{language: 'Mandarin', sound: 'wo1', register: 'colloquial'}, {language: 'Cantonese', sound: 'uk1'}]};
+charOne.meanings = [meaning1, meaning2];
+const charTwo = new Character;
+charTwo.glyph = "威";
+charTwo.strokes = 9;
+charTwo.learned = true;
+const charThree = new Character;
+charThree.glyph = "䢎";
+charThree.strokes = 7;
+charThree.learned = true;
+const charactersTest = [charOne, charTwo, charThree];
+
 module.exports = {
   index,
   show,
@@ -11,27 +31,14 @@ module.exports = {
 };
 
 function index(req, res) {
-  const charOne = new Character;
-  charOne.glyph = "喔";
-  charOne.strokes = 12;
-  charOne.learned = true;
-  const charTwo = new Character;
-  charTwo.glyph = "威";
-  charTwo.strokes = 9;
-  charTwo.learned = false;
-  const charThree = new Character;
-  charThree.glyph = "䢎";
-  charThree.strokes = 7;
-  charThree.learned = true;
-  const charactersTest = [charOne, charTwo, charThree];
   let learnedChars = 0;
   charactersTest.forEach(character => {if (character.learned) learnedChars++});
-  Character.find({}, (err, characters) => res.render('characters/index', {title: 'Characters', characters: charactersTest, learnedChars}));
+  Character.find({}, (err, characters) => res.render('characters/index', {title: 'Character List', characters: charactersTest, learnedChars}));
 }
 
 function show(req, res) {
   Character.findById(req.params.id, (err, character) => {
-    res.render('characters/show', {title: `Details of ${character.glyph}`, character});
+    res.render('characters/show', {title: `Character Details`, character: charOne});
   });
 }
 
--- models/character.js
+++ models/character.js
@@ -2,24 +2,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const readingSchema = new Schema({
-  language: {type: String, enum: ['Japanese', 'Mandarin', 'Cantonese', 'Taiwanese', 'Shanghainese', 'Hakka', 'Korean', 'Vietnamese', 'Middle Chinese', 'Old Chinese']},
-  register: {type: String, enum: ['n/a', 'native', 'Sinoxenic', 'colloquial', 'literary'], default: 'n/a'},
+  language: {type: String, enum: ['Japanese', 'Mandarin', 'Cantonese', 'Korean', 'Middle Chinese'], required: true},
+  register: {type: String, enum: ['native', 'Sinoxenic', 'colloquial', 'literary']},
   sound: {type: String, required: true}
-}, {
-  timestamps: true
 });
 
-const characterSchema = new Schema({
+const meaningSchema = new Schema({
+  definition: {type: String, required: true},
+  readings: [readingSchema]
+});
+
+const componentSchema = new Schema({
   glyph: {type: String, required: true},
+  role: {type: String, enum: ['Semantic', 'Phonetic', 'Phono-semantic'], required: true},
+  form: {type: String}
+})
+
+const characterSchema = new Schema({
+  glyph: {type: String, required: true, unique: true},
   learned: {type: Boolean, default: false},
-  semantic: [{type: Schema.Types.ObjectId, ref: 'Character'}],
-  phonetic: [{type: Schema.Types.ObjectId, ref: 'Character'}],
+  components: [componentSchema],
   strokes: {type: Number, min: 1, max: 99, required: true},
-  variants: {type: String, default: 'n/a'},
-  meaning: {type: String, default: 'n/a'},
-  readings: [readingSchema],
-}, {
-  timestamps: true
+  variants: {type: String},
+  meanings: [meaningSchema],
 });
 
 module.exports = mongoose.model('Character', characterSchema);
